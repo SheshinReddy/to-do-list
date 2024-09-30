ad3af0b4be9a245117e89f9afce67404d8324001
SheshinShivampeta <sheshin.voiceqube.com>
2024-09-30 18:19:55 +0530

    converting to ts

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+
+// Import MUI Roboto fonts globally
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const container = document.getElementById('root');
+
+if(container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+    <App />
+    </React.StrictMode>,
+  );
+} else {
+  console.error("Root element not found");
+}
