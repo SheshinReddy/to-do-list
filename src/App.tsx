a45553d85691e89aa579a0411e8140c6f4302750
SheshinShivampeta <sheshin.voiceqube.com>
2024-10-01 13:42:21 +0530

    Added raw table for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import AllTasks from './components/AllTasks.tsx';
 
 function App() {
   return (
     <div className="App">
-      
+      <AllTasks />
     </div>
   );
 }
